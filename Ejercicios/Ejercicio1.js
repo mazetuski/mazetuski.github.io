a6132e561a2a3304980a7ab55e820be745d7a9c1
mazetuski <migue_95_zb@hotmail.com>
2016-10-07 08:59:07 +0200

    Ejercicios 1-7

--- Ejercicios/Ejercicio1.js
+++ Ejercicios/Ejercicio1.js
@@ -2,4 +2,5 @@
  * Created by Miguel Ángel Zamora Blanco on 06/10/2016.
  */
 
+alert("Hola Mundo");
 alert("Soy el primer script");
--- Ejercicios/Ejercicio6.js
+++ Ejercicios/Ejercicio6.js
@@ -6,17 +6,15 @@ var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N',
     'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 
 var numeroDni=prompt("Número Dni: ");
-var letraDni=prompt("Letra Dni: ");
-var letraCalculada;
 
-switch (numeroDni){
-    case (numeroDni<1 || numeroDni>99999999):
-        alert("El numero es invalido");
-        break;
-    default:
-        letraCalculada=letras[numeroDni%23];
-        if(letraCalculada==letraDni.toUpperCase())
-            alert('El dni es correcto');
-        else
-            alert('El dni no es correcto');
+if(numeroDni<1 || numeroDni>99999999)
+    alert("Numero invalido");
+else{
+    var letraDni=prompt("Indroduzca la letra del dni: ");
+    letraCalculada=letras[numeroDni%23];
+    if(letraCalculada==letraDni.toUpperCase())
+        alert('El dni es correcto');
+    else
+        alert('El dni no es correcto');
 }
+
